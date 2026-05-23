d3480d3340a691ef1cc6d0edeba835825d24d58f
PrasanthRajuNPR <prasanthrajunidimoru@gmail.com>
2026-05-23 14:40:20 +0530

    Remove httpOnly from cookie for localhost compatibility

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,7 +6,6 @@ const generateToken = (res, payload) => {
   });
 
   res.cookie('token', token, {
-    httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 2 * 24 * 60 * 60 * 1000, // 2 days
